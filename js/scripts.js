9f0b36c9a9da9aeb3a17a6cd01667f0307f6711a
erastus <erastuskariuki15@gmail.com>
2020-10-07 18:00:29 +0300

    add chenges

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,9 @@ var add = function(number1, number2) {
   var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 alert(multiply(number1, number2));
+
+$(document).ready(function() {
+  var number1 = parseInt($("#add1").val());
+  var number2 = parseInt($("#add2").val());
+  alert(add(number1, number2));
+});
